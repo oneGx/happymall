c6976f4fc777a2beef51ac52bacd9dd0db3f08fa
onegx <287289950@qq.com>
2017-07-13 15:19:02 +0800

    project init

---
+++ src/main/java/com/happymall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.happymall.dao;
+
+import com.happymall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/happymall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.happymall.dao;
+
+import com.happymall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
